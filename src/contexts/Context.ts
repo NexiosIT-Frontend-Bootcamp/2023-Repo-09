6d17d3dddf9fc6ea343c6ab7fa282b98a894a430
Brecht <brechtvansteenweghen@gmail.com>
2023-12-06 13:39:55 +0100

    Add pages + Add context

---
+++ src/contexts/Context.ts
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+import { LoginResult } from "./LoginResult";
+
+export interface IUserContext {
+    loading: boolean;
+    user?: IUser;
+    jwt?: string;
+    signIn: (email: string, password: string) => Promise<LoginResult>;
+    signOut: () => void;
+}
+
+const UserContext = createContext<IUserContext | null>(null);
+
+export const UserContextProvider = ({ children }: IProviderProps) => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useState<IUser | undefined>();
+    const [jwt, setJwt] = useState<string>();
+
+    const signIn = async (email: string, password: string): Promise<LoginResult> => {
+        return { success: true };
+    };
+
+    const signOut = async () => {
+    };
+
+    return <UserContext.Provider value={{ loading, signIn, signOut, user, jwt }}>{children}</UserContext.Provider>;
+};
+
+// Create a custom hook to use the UserContext
+export const useUserContext = (): IUserContext => {
+    const context = useContext<IUserContext | null>(UserContext);
+    if (!context) {
+        throw new Error("useUserContext must be used within a UserContextProvider");
+    }
+    return context;
+};
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      {/* component code */}
+    </div>
+  );
+}
+
+export default Login;
