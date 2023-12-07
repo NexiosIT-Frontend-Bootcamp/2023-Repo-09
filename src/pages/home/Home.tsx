4b8254e2afb23f5430c61277b4bb30c16185f62b
Brecht <brechtvansteenweghen@gmail.com>
2023-12-07 15:33:53 +0100

    Fix home screen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { IUser } from '../../types/IUser';
-import { useUserContext } from '../../contexts/Context';
+import { useUserContext } from '../../contexts/UserContext';
 import { GetUser } from '../../api/User';
+import { Container, IconButton, List, ListItem, TextField, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { SendRounded } from '@mui/icons-material';
+
+const defaultTheme = createTheme();
 
 const Home: React.FC = () => {
 
@@ -10,8 +14,11 @@ const Home: React.FC = () => {
     const [users, setUsers] = useState<IUser[]>([]);
     const [usersLoading, setUsersLoading] = useState<boolean>();
     const [usersError, setUsersError] = useState<string>();
+    const [inputValue, setInputValue] = useState<string>('');
 
     useEffect(() => {
+		// const loadMessages
+			// await GetMessages
         const getAllUsers = async () => {
             if (!jwt) return [];
         
@@ -32,11 +39,59 @@ const Home: React.FC = () => {
         getAllUsers();
     }, [jwt]);
 
-    return (
-    <div>
-        Home
+	const [textboxes, setTextboxes] = useState<string[]>([]); // Initial state with an empty text box
+
+	const handleChange = (value: string) => {
+		const updatedTextboxes = [...textboxes, value];
+		setTextboxes(updatedTextboxes);
+    setInputValue('');
+	};
+
+	return (
+	<ThemeProvider theme={defaultTheme}>
+	<Container component="main" >
+  <div style={{ display: 'flex', height: '100vh' }}>
+      {/* Sidebar */}
+      <div style={{ width: '200px', padding: '20px', backgroundColor: '#f0f0f0', position: 'fixed', left: 0, top: 0, bottom: 0 }}>
+        <Typography variant="h6" style={{ marginBottom: '20px' }}>
+          Groups
+        </Typography>
+        <List>
+          {['test1', 'test2', 'test3'].map((group, index) => (
+            <ListItem button key={index}>
+              {group}
+            </ListItem>
+          ))}
+        </List>
+      </div>
+
+      {/* Main Content */}
+      <div style={{ width: '600px', marginLeft: '0px', padding: '20px' }}>
+        <div style={{ marginBottom: '10px', width: '300px' }}>
+          {textboxes.map((value, index) => (
+            <div key={index} style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px', width: '600px' }}>
+              <Typography variant="body1">{value}</Typography>
+            </div>
+          ))}
+        </div>
+    
+    <div style={{position: 'fixed', bottom: 0, left: 10, width: '100%', padding: '10px', backgroundColor: '#fff' }}>
+      <TextField
+        value={inputValue}
+        label={`Bottom Text Box`}
+        onChange={(e) => setInputValue(e.target.value)}
+        fullWidth id="fullWidth"
+        style={{maxWidth: '600px'}}
+      />
+		<IconButton color="primary" onClick={() => { handleChange(inputValue)}} aria-label="send message">
+			<SendRounded style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} />
+		</IconButton>
+      </div>
     </div>
-    );
+    </div>
+	</Container>
+	</ThemeProvider>
+	);
 }
 
 export default Home;
